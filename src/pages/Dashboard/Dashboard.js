f88abe891dddb2f5e5c5076d182fd83bf95a9cf7
Zohorl <zohorul109561@gmail.com>
2022-05-23 09:45:49 +0600

    added react router and query

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2 className='text-3xl text-purple-500 text-center'>Welcome To Dashboard </h2>
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/pages/Purchase/Purschase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Purschase = () => {
+    return (
+        <div>
+            <h2 className='text-center'>Purchese Here</h2>
+        </div>
+    );
+};
+
+export default Purschase;
