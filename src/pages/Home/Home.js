f2229119ba7d469d5b55fbc74a3fab7b3cbd0244
Zohorl <zohorul109561@gmail.com>
2022-05-23 12:17:54 +0600

    added info section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Banner from './Banner';
+import Info from './Info';
 import Services from './Services';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <div className='max-w-7xl mx-auto mx-12'>
+            <div className='max-w-7xl mx-12'>
                 <Services></Services>
+                <Info></Info>
             </div>
         </div>
     );
--- src/pages/Home/Info.js
+++ src/pages/Home/Info.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import InfoCard from './InfoCard';
+import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
+
 
 const Info = () => {
     return (
-        <div>
-
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-36'>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hours " description="Lorem Ipsum is simply dummy text of the pri" img={clock}></InfoCard>
+            <InfoCard bgClass="bg-accent" cardTitle="Visit Our Location " description="Brooklyn, NY 10036, United States" img={marker}></InfoCard>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact Us Now" description="+000 123 456789" img={phone}></InfoCard>
         </div>
     );
 };
